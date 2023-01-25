5280313f0c199d6259fd18b2c50f4160c43f3258
Rohit Tokala <rtokala@purdue.edu>
2023-01-24 20:16:33 -0500

    Make loading a user page better

--- src/pages/user/[user].tsx
+++ src/pages/user/[user].tsx
@@ -12,26 +12,32 @@ const User = () => {
   return (
     <div className=" flex flex-col items-center">
       <div className=" flex w-full gap-5 rounded-md border border-primary p-4 text-white md:w-4/5 lg:w-2/5">
-        <Image
-          src={
-            isLoading
-              ? "https://media.istockphoto.com/id/1332100919/vector/man-icon-black-icon-person-symbol.jpg?s=612x612&w=is&k=20&c=3g5FPg9un7Ktq2_TUpKqpnTL9WpSvNB0SzN9RrXSUog="
-              : userData
-              ? userData?.image!
-              : "https://media.istockphoto.com/id/1332100919/vector/man-icon-black-icon-person-symbol.jpg?s=612x612&w=is&k=20&c=3g5FPg9un7Ktq2_TUpKqpnTL9WpSvNB0SzN9RrXSUog="
-          }
-          alt={`${userData?.name}'s profile picture`}
-          width={70}
-          height={70}
-          className="rounded-full"
-        />
-        <div className=" text-xl font-medium text-white">{`u/${userData?.name}`}</div>
-        <div className=" flex flex-col gap-2">
-          <div>{`${userData && userData?.posts.length} posts`}</div>
-          <div>{`${userData && userData.comments.length} comments`}</div>
-        </div>
+        {isLoading ? (
+          <div></div>
+        ) : (
+          <>
+            <Image
+              src={
+                isLoading
+                  ? "https://media.istockphoto.com/id/1332100919/vector/man-icon-black-icon-person-symbol.jpg?s=612x612&w=is&k=20&c=3g5FPg9un7Ktq2_TUpKqpnTL9WpSvNB0SzN9RrXSUog="
+                  : userData
+                  ? userData?.image!
+                  : "https://media.istockphoto.com/id/1332100919/vector/man-icon-black-icon-person-symbol.jpg?s=612x612&w=is&k=20&c=3g5FPg9un7Ktq2_TUpKqpnTL9WpSvNB0SzN9RrXSUog="
+              }
+              alt={`${userData?.name}'s profile picture`}
+              width={70}
+              height={70}
+              className="rounded-full"
+            />
+            <div className=" text-xl font-medium text-white">{`u/${userData?.name}`}</div>
+            <div className=" flex flex-col gap-2">
+              <div>{`${userData && userData?.posts.length} posts`}</div>
+              <div>{`${userData && userData.comments.length} comments`}</div>
+            </div>
+          </>
+        )}
       </div>
-      <Posts where={{ userId }} />;
+      {!isLoading && <Posts where={{ userId }} />};
     </div>
   );
 };
